e7f7a2add6633e66b88bb7858537fd8282c2705a
Alex Johnson <alexjo5000@gmail.com>
2017-08-20 12:09:49 -0400

    working Alexa Skill
    * assets is info from the Alexa skill
    * src is for the lambda function

---
+++ src/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+var api = require('./jenkinsRESTHandler.js');
+
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'Jobstatus': function () {
+        this.emit(':tell', "job not found");
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
